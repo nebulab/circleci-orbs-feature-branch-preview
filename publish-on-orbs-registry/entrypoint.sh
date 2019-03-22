7364878e5184fe994a88884c1b394eaee5f6b33e
Alberto Vena <kennyadsl@gmail.com>
2019-03-22 08:25:14 +0100

    Use a single command to publish CircleCI orb
    This way we are not going to store the secret token on a file
    of the Docker instance ran by the GitHub Action

--- publish-on-orbs-registry/entrypoint.sh
+++ publish-on-orbs-registry/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 
-# Setup using the secret CIRCLECI_TOKEN
-circleci setup --no-prompt --token $CIRCLECI_TOKEN
-
 # Publish the orb on CircleCI Orbs Registry
-circleci orb publish source.yml nebulab/feature-branch-preview@$GITHUB_REF
+circleci orb publish source.yml nebulab/feature-branch-preview@$GITHUB_REF --token $CIRCLECI_TOKEN
