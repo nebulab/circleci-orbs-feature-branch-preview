c1ccee5e3912eb58f6f16ea4a0246aceea20b0e9
Alberto Vena <kennyadsl@gmail.com>
2019-03-22 11:05:56 +0100

    echo the version that is going to published in the GH Action

--- publish-on-orbs-registry/entrypoint.sh
+++ publish-on-orbs-registry/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-# Publish the orb on CircleCI Orbs Registry
+echo "Trying to publish orb with version ${GITHUB_REF}"
 circleci orb publish source.yml "nebulab/feature-branch-preview@${GITHUB_REF}" --token $CIRCLECI_TOKEN
