edf94a81999f81c61eed345a748dd43af748d8cc
Alberto Vena <kennyadsl@gmail.com>
2019-03-22 08:33:53 +0100

    Correctly interpolate GITHUB_REF env variable

--- publish-on-orbs-registry/entrypoint.sh
+++ publish-on-orbs-registry/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 # Publish the orb on CircleCI Orbs Registry
-circleci orb publish source.yml nebulab/feature-branch-preview@$GITHUB_REF --token $CIRCLECI_TOKEN
+circleci orb publish source.yml "nebulab/feature-branch-preview@${GITHUB_REF}" --token $CIRCLECI_TOKEN
