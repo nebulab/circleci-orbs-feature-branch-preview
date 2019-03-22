16faa986b18ec737b2426e364e6849f5779da73c
Alberto Vena <kennyadsl@gmail.com>
2019-03-22 11:39:02 +0100

    Extract the version from $GITHUB_REF
    $GITHUB_REF is something like "refs/tags/1.6.2" and we need to take
    only the last part of it.

--- publish-on-orbs-registry/entrypoint.sh
+++ publish-on-orbs-registry/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
-echo "Trying to publish orb with version ${GITHUB_REF}"
-circleci orb publish source.yml "nebulab/feature-branch-preview@${GITHUB_REF}" --token $CIRCLECI_TOKEN
+# $GITHUB_REF is something like "refs/tags/1.6.2" and we need to take
+# only the last part of it.
+version=${GITHUB_REF/refs\/tags\//}
+
+echo "Trying to publish orb with version ${version}"
+circleci orb publish source.yml "nebulab/feature-branch-preview@${version}" --token $CIRCLECI_TOKEN
